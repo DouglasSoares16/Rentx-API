79dcb3ef8810002433a402217440ec0745e99e4a
Douglas Soares <douglasmaismcpe@gmail.com>
2021-09-27 09:29:06 -0300

    Completed Chapter IV

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -27,7 +27,7 @@ describe("Create Category Controller", () => {
         await connection.close();
     });
 
-    it("Should be able to create a new category", async () => {
+    it("Should be able to list all categories", async () => {
         const responseToken = await request(app).post("/sessions").send({
             email: "admin@rentalx.com.br",
             password: "admin",
@@ -35,7 +35,7 @@ describe("Create Category Controller", () => {
 
         const { token } = responseToken.body;
 
-        const response = await request(app)
+        await request(app)
             .post("/categories")
             .send({
                 name: "Category Supertest",
@@ -45,27 +45,13 @@ describe("Create Category Controller", () => {
                 Authorization: `Bearer ${token}`,
             });
 
-        expect(response.status).toBe(201);
-    });
+        const response = await request(app).get("/categories");
 
-    it("Should not be able to create a new category with name exists", async () => {
-        const responseToken = await request(app).post("/sessions").send({
-            email: "admin@rentalx.com.br",
-            password: "admin",
-        });
+        console.log(response.body);
 
-        const { token } = responseToken.body;
-
-        const response = await request(app)
-            .post("/categories")
-            .send({
-                name: "Category Supertest",
-                description: "Description Supertest",
-            })
-            .set({
-                Authorization: `Bearer ${token}`,
-            });
-
-        expect(response.status).toBe(400);
+        expect(response.status).toBe(200);
+        expect(response.body.length).toBe(1);
+        expect(response.body[0]).toHaveProperty("id");
+        expect(response.body[0].name).toEqual("Category Supertest");
     });
 });
--- src/shared/infra/typeorm/connection.ts
+++ src/shared/infra/typeorm/connection.ts
@@ -1,7 +1,17 @@
-import { Connection, createConnection } from "typeorm";
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
 createConnection();
 
-export default async (): Promise<Connection> => {
-    return createConnection();
+export default async (host = "localhost"): Promise<Connection> => {
+    const defaultOptions = await getConnectionOptions();
+
+    return createConnection(
+        Object.assign(defaultOptions, {
+            host,
+            database:
+                process.env.NODE_ENV === "test"
+                    ? "rentx_test"
+                    : defaultOptions.database,
+        })
+    );
 };
