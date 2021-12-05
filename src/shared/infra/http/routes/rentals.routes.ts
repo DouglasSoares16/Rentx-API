8bb19072a2901e782a1f3d250df071bf149f7566
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-05 14:24:27 -0300

    Created route of devolution rental

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 const rentalRouter = Router();
 
 rentalRouter.post("/", ensureAuthenticated, createRentalController.handle);
+rentalRouter.post(
+    "/devolution/:id",
+    ensureAuthenticated,
+    devolutionRentalController.handle
+);
 
 export { rentalRouter };
