b0607d4e58dfb61cacc671eb3a887baeee4d2237
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-05 11:28:25 -0300

    Added compareInDays functionality

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -2,6 +2,7 @@ interface IDateProvider {
     compareInHours(start_date: Date, end_date: Date): number;
     convertToUTC(date: Date): string;
     dateNow(): Date;
+    compareInDays(start_date: Date, end_date: Date): number;
 }
 
 export { IDateProvider };
