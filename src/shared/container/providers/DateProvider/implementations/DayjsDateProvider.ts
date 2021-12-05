311d6937adb5a4d955fac10a79a846f9a72a549b
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-05 11:30:48 -0300

    Implemented the functionality to compareInDays

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -6,11 +6,18 @@ import { IDateProvider } from "../IDateProvider";
 dayjs.extend(utc);
 
 class DayjsDateProvider implements IDateProvider {
-    compareInHours(start_date: Date, end_date: Date): number {
+    compareInDays(start_date: Date, end_date: Date): number {
         const end_date_utc = this.convertToUTC(end_date);
         const start_date_utc = this.convertToUTC(start_date);
 
         return dayjs(end_date_utc).diff(start_date_utc, "hours");
+    }
+
+    compareInHours(start_date: Date, end_date: Date): number {
+        const end_date_utc = this.convertToUTC(end_date);
+        const start_date_utc = this.convertToUTC(start_date);
+
+        return dayjs(end_date_utc).diff(start_date_utc, "days");
     }
 
     convertToUTC(date: Date): string {
