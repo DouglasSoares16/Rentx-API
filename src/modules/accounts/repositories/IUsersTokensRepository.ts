a4d3226d0178034f85210594bb3f10420310dc71
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-08 11:00:13 -0300

    Added 2 methods: deleteById and findByUserAndByToken to the UsersTokens interface

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -7,6 +7,13 @@ interface IUsersTokensRepository {
         user_id,
         refresh_token,
     }: ICreateUserTokenDTO): Promise<UserTokens>;
+
+    findByUserIdAndRefreshToken(
+        user_id: string,
+        refresh_token: string
+    ): Promise<UserTokens>;
+
+    deleteById(id: string): Promise<void>;
 }
 
 export { IUsersTokensRepository };
