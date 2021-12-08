fa8612c05babcc90863465fec1daf71d9201ffb0
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-08 20:05:48 -0300

    Refactored ensureAuthenticated

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 
+import auth from "@config/auth";
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
@@ -15,6 +17,8 @@ export async function ensureAuthenticated(
 ) {
     const authHeader = request.headers.authorization;
 
+    const userTokensRepository = new UsersTokensRepository();
+
     if (!authHeader) {
         throw new AppError("Token missing!", 401);
     }
@@ -24,12 +28,13 @@ export async function ensureAuthenticated(
     try {
         const { sub: user_id } = verify(
             token,
-            "ff8139ffc3af454ff1f488ed14008aba"
+            auth.secret_refresh_token
         ) as IPayload;
 
-        const userRepository = new UsersRepository();
-
-        const user = await userRepository.findById(user_id);
+        const user = await userTokensRepository.findByUserIdAndRefreshToken(
+            user_id,
+            token
+        );
 
         if (!user) {
             throw new AppError("User does not exists!", 404);
