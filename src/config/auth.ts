22b473091b6e986554d6fe27c4fb307de739e51d
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-08 09:25:53 -0300

    Created configuration of authentication

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "ff8139ffc3af454ff1f488ed14008aba",
+    expires_in_token: "15m",
+    secret_refresh_token: "a62d0b533c138cbdf241e588a2a9de39",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30,
+};
