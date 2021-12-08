23925746f3384f1a890bd0cffe2bccffae66e32f
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-08 11:10:57 -0300

    Added decorator injectable

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -1,5 +1,5 @@
 import { sign, verify } from "jsonwebtoken";
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
 import auth from "@config/auth";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
@@ -11,6 +11,7 @@ interface IPayload {
     email: string;
 }
 
+@injectable()
 class RefreshTokenUseCase {
     constructor(
         @inject("UsersTokensRepository")
