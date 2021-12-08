444edce6f17403e5a60412a5addda8c0f8645399
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-08 08:16:11 -0300

    Implemented UsersTokens Repository

---
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,32 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+
+import { UserTokens } from "../entities/UserTokens";
+
+class UsersTokensRepository implements IUsersTokensRepository {
+    private repository: Repository<UserTokens>;
+
+    constructor() {
+        this.repository = getRepository(UserTokens);
+    }
+
+    async create({
+        expires_date,
+        user_id,
+        refresh_token,
+    }: ICreateUserTokenDTO): Promise<UserTokens> {
+        const refresh = this.repository.create({
+            refresh_token,
+            user_id,
+            expires_date,
+        });
+
+        await this.repository.save(refresh);
+
+        return refresh;
+    }
+}
+
+export { UsersTokensRepository };
