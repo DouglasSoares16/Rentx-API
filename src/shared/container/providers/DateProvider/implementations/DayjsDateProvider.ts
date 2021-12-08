bee203ffeb8a1a9d6a98170d2909b0294c7afd8c
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-08 09:29:40 -0300

    Implemented addDays in DateProvider

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -6,6 +6,10 @@ import { IDateProvider } from "../IDateProvider";
 dayjs.extend(utc);
 
 class DayjsDateProvider implements IDateProvider {
+    addDays(days: number): Date {
+        return dayjs().add(days, "days").toDate();
+    }
+
     compareInDays(start_date: Date, end_date: Date): number {
         const end_date_utc = this.convertToUTC(end_date);
         const start_date_utc = this.convertToUTC(start_date);
