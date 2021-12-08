1e8c396cd10a279589772d5e979fb2129da0c7d6
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-08 19:48:07 -0300

    Created RefreshToken controller

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { AuthenticateUserController } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+import { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
 
 const authenticateRouter = Router();
 
 const authenticateUserController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
 
 authenticateRouter.post("/sessions", authenticateUserController.handle);
+authenticateRouter.post("/refresh-token", refreshTokenController.handle);
 
 export { authenticateRouter };
