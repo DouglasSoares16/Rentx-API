263ab02967ace756882d323410779019bbce2693
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-08 21:51:11 -0300

    Added register of MailProvider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,8 +2,15 @@ import { container } from "tsyringe";
 
 import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
+import { IMailProvider } from "./MailProvider/IMailProvider";
+import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 
 container.registerSingleton<IDateProvider>(
     "DayjsDateProvider",
     DayjsDateProvider
+);
+
+container.registerInstance<IMailProvider>(
+    "EtherealMailProvider",
+    new EtherealMailProvider()
 );
