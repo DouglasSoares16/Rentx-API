d694562bdba9fa9e0357c3c5440dcfccb97139d6
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-11 11:28:26 -0300

    Changed tests: using refresh_token

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -33,7 +33,7 @@ describe("Create Category Controller", () => {
             password: "admin",
         });
 
-        const { token } = responseToken.body;
+        const { refresh_token } = responseToken.body;
 
         await request(app)
             .post("/categories")
@@ -42,7 +42,7 @@ describe("Create Category Controller", () => {
                 description: "Description Supertest",
             })
             .set({
-                Authorization: `Bearer ${token}`,
+                Authorization: `Bearer ${refresh_token}`,
             });
 
         const response = await request(app).get("/categories");
