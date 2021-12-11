529a1915ed2246e55482a7a78b8ef2b036481881
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-11 09:27:23 -0300

    Implemented findByRefreshToken method to repository

--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -12,6 +12,16 @@ class UsersTokensRepository implements IUsersTokensRepository {
         this.repository = getRepository(UserTokens);
     }
 
+    async findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+        const userToken = await this.repository.findOne({
+            where: {
+                refresh_token,
+            },
+        });
+
+        return userToken;
+    }
+
     async deleteById(id: string): Promise<void> {
         await this.repository.delete(id);
     }
