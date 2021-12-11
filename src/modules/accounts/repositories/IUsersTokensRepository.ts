9842f988f8f5e1f2ebb634387635f3b738945218
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-11 09:26:29 -0300

    Added method findByRefreshToken

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -14,6 +14,8 @@ interface IUsersTokensRepository {
     ): Promise<UserTokens>;
 
     deleteById(id: string): Promise<void>;
+
+    findByRefreshToken(refresh_token: string): Promise<UserTokens>;
 }
 
 export { IUsersTokensRepository };
