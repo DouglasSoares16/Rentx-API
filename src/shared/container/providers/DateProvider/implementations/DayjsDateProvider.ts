ce0f245d0ff45c96f3149c3e8cfbdbbcfa1256e2
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-11 09:25:42 -0300

    Implemented compareIfBefore method to provider

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -6,6 +6,10 @@ import { IDateProvider } from "../IDateProvider";
 dayjs.extend(utc);
 
 class DayjsDateProvider implements IDateProvider {
+    compareIfBefore(start_date: Date, end_date: Date): boolean {
+        return dayjs(start_date).isBefore(end_date);
+    }
+
     addHours(hours: number): Date {
         return dayjs().add(hours, "hour").toDate();
     }
