f2730d8f515dee3f06511cb9a90d918c32200816
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-17 10:47:04 -0300

    Middleware of authentication refatored

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 
 import auth from "@config/auth";
-import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
@@ -16,8 +15,6 @@ export async function ensureAuthenticated(
 ) {
     const authHeader = request.headers.authorization;
 
-    const userTokensRepository = new UsersTokensRepository();
-
     if (!authHeader) {
         throw new AppError("Token missing!", 401);
     }
@@ -25,19 +22,7 @@ export async function ensureAuthenticated(
     const [, token] = authHeader.split(" ");
 
     try {
-        const { sub: user_id } = verify(
-            token,
-            auth.secret_refresh_token
-        ) as IPayload;
-
-        const user = await userTokensRepository.findByUserIdAndRefreshToken(
-            user_id,
-            token
-        );
-
-        if (!user) {
-            throw new AppError("User does not exists!", 404);
-        }
+        const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
         request.user = {
             id: user_id,
