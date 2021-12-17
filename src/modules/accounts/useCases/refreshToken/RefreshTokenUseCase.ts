0fb81063ab56a4516aaa0aa28dd5c45e9200dfd6
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-17 10:45:42 -0300

    Refresh token generates a new token for the user

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -11,6 +11,11 @@ interface IPayload {
     email: string;
 }
 
+interface ITokenResponse {
+    token: string;
+    refresh_token: string;
+}
+
 @injectable()
 class RefreshTokenUseCase {
     constructor(
@@ -21,7 +26,7 @@ class RefreshTokenUseCase {
         private dateProvider: IDateProvider
     ) {}
 
-    async execute(token: string): Promise<string> {
+    async execute(token: string): Promise<ITokenResponse> {
         const { sub: user_id, email } = verify(
             token,
             auth.secret_refresh_token
@@ -60,7 +65,15 @@ class RefreshTokenUseCase {
             user_id,
         });
 
-        return refresh_token;
+        const newToken = sign({}, auth.secret_token, {
+            subject: user_id,
+            expiresIn: auth.expires_in_token,
+        });
+
+        return {
+            token: newToken,
+            refresh_token,
+        };
     }
 }
 
