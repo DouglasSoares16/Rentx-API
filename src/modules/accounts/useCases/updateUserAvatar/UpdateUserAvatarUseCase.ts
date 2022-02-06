96f466d77852838cbfef33e0c14d3c8ad52e33ce
Douglas Soares <douglasmaismcpe@gmail.com>
2022-02-06 14:07:13 -0300

    The Upload Avatar Use Case has been Refactored: Using the LocalStorage Provider

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
-import { deleteFile } from "@utils/file";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 interface IRequest {
     user_id: string;
@@ -12,17 +12,22 @@ interface IRequest {
 class UpdateUserAvatarUseCase {
     constructor(
         @inject("UsersRepository")
-        private userRepository: IUsersRepository
+        private userRepository: IUsersRepository,
+
+        @inject("StorageProvider")
+        private storageProvider: IStorageProvider
     ) {}
 
     async execute({ user_id, avatar_file }: IRequest): Promise<void> {
         const user = await this.userRepository.findById(user_id);
 
         if (user.avatar) {
-            await deleteFile(`./tmp/avatar/${user.avatar}`);
+            await this.storageProvider.delete(user.avatar, "avatar");
         }
 
         user.avatar = avatar_file;
+
+        await this.storageProvider.save(avatar_file, "avatar");
 
         await this.userRepository.create(user);
     }
