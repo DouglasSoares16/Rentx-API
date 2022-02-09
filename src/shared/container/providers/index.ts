d2b9efc456ced82f5a5c8498d9cec6217a7c4559
Douglas Soares <douglasmaismcpe@gmail.com>
2022-02-09 20:33:52 -0300

    Storage Provider Management

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -5,6 +5,7 @@ import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvi
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 container.registerSingleton<IDateProvider>(
@@ -17,7 +18,12 @@ container.registerInstance<IMailProvider>(
     new EtherealMailProvider()
 );
 
+const diskStorage = {
+    local: LocalStorageProvider,
+    s3: S3StorageProvider,
+};
+
 container.registerSingleton<IStorageProvider>(
     "StorageProvider",
-    LocalStorageProvider
+    diskStorage[process.env.disk]
 );
