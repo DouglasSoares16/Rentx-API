b05fcc7adb9c3feb86b32a3f39df3647f9203dd0
Douglas Soares <douglasmaismcpe@gmail.com>
2022-02-10 20:30:12 -0300

    Created Route of Get Profile

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from "multer";
 
 import uploadConfig from "@config/upload";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUserUseCase/ProfileUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
@@ -11,6 +12,7 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 const userRoutes = Router();
 
@@ -22,5 +24,7 @@ userRoutes.patch(
     uploadAvatar.single("avatar"),
     updateUserAvatarController.handle
 );
+
+userRoutes.get("/profile", ensureAuthenticated, profileUserController.handle);
 
 export { userRoutes };
