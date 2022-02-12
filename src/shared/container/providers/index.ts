8495dbdae834f4e5b2b510a0074ae13ddf362b76
Douglas Soares <douglasmaismcpe@gmail.com>
2022-02-12 14:30:26 -0300

    The Container of Providers has been refactored

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,29 +1,3 @@
-import { container } from "tsyringe";
-
-import { IDateProvider } from "./DateProvider/IDateProvider";
-import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
-import { IMailProvider } from "./MailProvider/IMailProvider";
-import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
-import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
-import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
-import { IStorageProvider } from "./StorageProvider/IStorageProvider";
-
-container.registerSingleton<IDateProvider>(
-    "DayjsDateProvider",
-    DayjsDateProvider
-);
-
-container.registerInstance<IMailProvider>(
-    "EtherealMailProvider",
-    new EtherealMailProvider()
-);
-
-const diskStorage = {
-    local: LocalStorageProvider,
-    s3: S3StorageProvider,
-};
-
-container.registerSingleton<IStorageProvider>(
-    "StorageProvider",
-    diskStorage[process.env.disk]
-);
+import "./MailProvider";
+import "./StorageProvider";
+import "./DateProvider";
