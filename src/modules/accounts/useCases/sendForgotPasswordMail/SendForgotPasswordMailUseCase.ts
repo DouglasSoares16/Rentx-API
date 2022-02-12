f4f4c2f13212b90744a35122b486436915295268
Douglas Soares <douglasmaismcpe@gmail.com>
2022-02-12 14:28:03 -0300

    Changed name of Injection the Email Provider

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -20,7 +20,7 @@ class SendForgotPasswordMailUseCase {
         @inject("DayjsDateProvider")
         private dateProvider: IDateProvider,
 
-        @inject("EtherealMailProvider")
+        @inject("MailProvider")
         private mailProvider: IMailProvider
     ) {}
 
